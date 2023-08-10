d3ff1425550c71f0e0b2a66129efbf3658ba657f
Bosun18 <beliy-81@mail.ru>
2023-08-10 13:47:35 +0300

    changed calc

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -2,24 +2,25 @@
 
 namespace BrainGames\Games\Calc;
 
+use Exception;
+
 use function BrainGames\Engine\startGame;
 
 use const BrainGames\Engine\NUMBER_OF_ROUNDS;
 
 const RULE = 'What is the result of the expression?';
 
+/**
+ * @throws Exception
+ */
 function calc(string $operator, int $num1, int $num2): string
 {
-    switch ($operator) {
-        case '+':
-            return $num1 + $num2;
-        case '-':
-            return $num1 - $num2;
-        case '*':
-            return $num1 * $num2;
-        default:
-            return false;
-    }
+    return match ($operator) {
+        '+' => $num1 + $num2,
+        '-' => $num1 - $num2,
+        '*' => $num1 * $num2,
+        default => throw new Exception('Неизвестный оператор'),
+    };
 }
 
 function run(): void
