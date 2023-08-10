0b61f9d8c13501567a5c67f96f18ea6c5d2e9377
Bosun18 <beliy-81@mail.ru>
2023-08-10 14:09:57 +0300

    changed gcd

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -8,7 +8,7 @@ use const BrainGames\Engine\NUMBER_OF_ROUNDS;
 
 const RULE = 'Find the greatest common divisor of given numbers.';
 
-function getMaxDivisor(int $num1, int $num2): ?string
+function getMaxDivisor(int $num1, int $num2): string
 {
     $min = min($num1, $num2);
     for ($i = $min; $i >= 1; $i -= 1) {
@@ -16,7 +16,7 @@ function getMaxDivisor(int $num1, int $num2): ?string
             return $i;
         }
     }
-    return null;
+    return '1';
 }
 
 function run(): void
